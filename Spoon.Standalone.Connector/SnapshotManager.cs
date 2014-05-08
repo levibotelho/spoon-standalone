99109f06267f142afc41c21776c49cc6e71a12a5
LeviBotelho <levi_botelho@hotmail.com>
2014-05-08 23:11:51 +0200

    Preliminary implementation of SnapshotManager (untested)

--- Spoon.Standalone.Connector/SnapshotManager.cs
+++ Spoon.Standalone.Connector/SnapshotManager.cs
@@ -1,12 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Net.Http;
 using System.Threading.Tasks;
+using System.Web.Mvc;
 
 namespace Spoon.Standalone.Connector
 {
-    public class SnapshotManager
+    public static class SnapshotManager
     {
+        public static async Task<ContentResult> GetSnapshotAsync(string escapedFragment, string azureStorageAccount, string azureStorageContainer)
+        {
+            var blobUrl = GetSnapshotUrl(escapedFragment, azureStorageAccount, azureStorageContainer);
+            using (var client = new HttpClient())
+            {
+                return new ContentResult
+                {
+                    Content = await client.GetStringAsync(blobUrl),
+                    ContentType = "text/html"
+                };
+            }
+        }
+
+        static string GetSnapshotUrl(string escapedFragment, string azureStorageAccount, string azureStorageContainer)
+        {
+            return string.Format(
+                "http://{0}.blob.core.windows.net/{1}/_escaped_fragment_={2}.html",
+                azureStorageAccount,
+                azureStorageContainer,
+                escapedFragment);
+        }
     }
 }
