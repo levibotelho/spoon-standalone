e68d883daa5433774b2eab39ef82f3eae2f18268
LeviBotelho <levi_botelho@hotmail.com>
2014-05-18 12:04:14 +0200

    Fixed critical homepage snapshot naming bug

--- Spoon.Standalone/src/blob-name-generator.js
+++ Spoon.Standalone/src/blob-name-generator.js
@@ -2,6 +2,5 @@
 exports.generate = function(url) {
     var hashbangIndex = url.indexOf("#!");
     var name = url.substr(hashbangIndex + 2);
-    if (name == '/') name = '';
     return hashbangIndex == -1 ? namePrefix + '.html' : namePrefix + name + '.html';
 };
